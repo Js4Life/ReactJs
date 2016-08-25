f8f5b3bcfe7a5ea00a645149bba39a63e8f3a42a
Sagir <sagir.sagir@gmail.com>
2016-08-25 13:29:19 +0530

    Fetching base url using play framework for root

--- modules/parabole-module-ui/app/com/parabole/ui/controllers/HomeController.java
+++ modules/parabole-module-ui/app/com/parabole/ui/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.parabole.ui.controllers;
 import com.parabole.auth.controllers.AuthController;
 import com.parabole.ui.views.html.index;
 import com.parabole.ui.views.html.main;
+import play.Configuration;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.mvc.Controller;
@@ -19,12 +20,16 @@ public class HomeController extends Controller {
     @Inject
     AuthController authController;
 
+    @Inject
+    Configuration configuration;
+
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
+
     public Result index() {
         return ok(index.render("Your new application is ready."));
     }
@@ -34,8 +39,10 @@ public class HomeController extends Controller {
         final String userId = requestData.get("userid");
         final String password = requestData.get("password");
         Boolean status = authController.login(userId, password);
+        String pageTitle = "Parabole Platform";
+        String baseUrl = configuration.getString("application.baseUrl");
         if(status)
-            return ok(main.render("Parabole Platform"));
+            return ok(main.render(pageTitle, baseUrl));
         else
             return index();
     }
