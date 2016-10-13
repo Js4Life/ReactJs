5fad7306c70a37e5e45c36c6a2a610747695bb40
Atanu <atanu111987@gmail.com>
2016-10-13 19:17:03 +0530

    basel regulation init

--- modules/parabole-module-cecl/public/js/services.js
+++ modules/parabole-module-cecl/public/js/services.js
@@ -115,7 +115,9 @@ angular.module('RDAApp.services', [])
         'GetChecklistByParagraph' : 'getChecklistByParagraph',
         'GetChecklistBySection' : 'getChecklistBySection',
         'GetChecklistBySubtopic' : 'getChecklistBySubtopic',
-        'GetChecklistByTopic' : 'getChecklistByTopic'
+        'GetChecklistByTopic' : 'getChecklistByTopic',
+
+        'GetRegulations' : 'getRegulations'
     };
     SharedService.chartDataMap = {
         '0' : 'getHardCodedResponse/chartData1',
@@ -832,6 +834,10 @@ angular.module('RDAApp.services', [])
     SharedService.getCommentAttachmentById = function (id) {
         var sendObj = {"id": id};
         return SharedService.invokeService('GetCommentAttachmentById', sendObj, 'post');
+    }
+
+    SharedService.getRegulations = function(){
+        return SharedService.invokeService('GetRegulations');
     }
 
     return SharedService;
